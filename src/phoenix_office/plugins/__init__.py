6018965b9b6cace2d4d1e939f6b0a8cf2d35e9ef
Schloss87 <126344020+Schloss87@users.noreply.github.com>
2026-06-26 12:22:48 -0500

    Export plugin capability registry helpers

--- src/phoenix_office/plugins/__init__.py
+++ src/phoenix_office/plugins/__init__.py
@@ -1,5 +1,15 @@
 """Phoenix Office plugin capability metadata."""
 
 from phoenix_office.plugins.office import get_office_plugin_capabilities
+from phoenix_office.plugins.registry import (
+    get_plugin_capability_by_id,
+    get_registered_plugin_capabilities,
+    validate_unique_capability_ids,
+)
 
-__all__ = ["get_office_plugin_capabilities"]
+__all__ = [
+    "get_office_plugin_capabilities",
+    "get_plugin_capability_by_id",
+    "get_registered_plugin_capabilities",
+    "validate_unique_capability_ids",
+]
